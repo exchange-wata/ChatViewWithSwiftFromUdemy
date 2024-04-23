8ad988687d730e35ee2ae46c702fe4821a0cd77c
exchange-wata <exchange.wata0907@gmail.com>
2024-04-23 20:15:13 +0900

    section3: ファイル設定

---
+++ ChatViewWithSwiftUI/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by 渡辺瑞樹 on 2024/04/23.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
