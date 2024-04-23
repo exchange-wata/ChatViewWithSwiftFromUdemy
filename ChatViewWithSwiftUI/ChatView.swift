616b92ca9edb3218fc4dbb53a128adb38bae8db5
exchange-wata <exchange.wata0907@gmail.com>
2024-04-23 20:15:35 +0900

    section3: チャット画面の大まかなレイアウト

--- ChatViewWithSwiftUI/ChatView.swift
+++ ChatViewWithSwiftUI/ChatView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct ChatView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            // Message Area
+            ScrollView {
+                VStack(spacing: 0) {
+                    ForEach(0..<15) {
+                        _ in
+                        HStack {
+                            Circle()
+                                .frame(width: 60, height: 60)
+                            Capsule()
+                                .frame(height: 60)
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 72)
+            }
+            .background(.teal)
+        }
     }
 }
 
