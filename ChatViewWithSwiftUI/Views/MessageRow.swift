41ab80948ba9bbae70f4cb275a2bc7f59c674400
exchange-wata <exchange.wata0907@gmail.com>
2024-04-24 15:10:35 +0900

    section5: message area のUI

--- ChatViewWithSwiftUI/Views/MessageRow.swift
+++ ChatViewWithSwiftUI/Views/MessageRow.swift
@@ -9,18 +9,37 @@ import SwiftUI
 
 struct MessageRow: View {
     var body: some View {
-        HStack {
-            Circle()
-                .frame(width: 60, height: 60)
-            Capsule()
-                .frame(height: 60)
+        HStack(alignment: .top) {
+            Image(systemName: "person.circle")
+                .resizable()
+                .frame(width: 48, height: 48)
+            Text("hellohellohellohellohellohellohellohellohellohellohellohellohellohello")
+                .padding()
+                .background(.white)
+                .cornerRadius(30)
+            VStack(alignment: .trailing) {
+                Spacer()
+                Text("既読")
+                Text(formattedDateString)
+            }
+            .foregroundColor(.secondary)
+            .font(.footnote)
+            
+            Spacer()
         }
         .padding(.bottom)
+    }
+    
+    private var formattedDateString: String {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: Date())
     }
 }
 
 struct MessageRow_Previews: PreviewProvider {
     static var previews: some View {
         MessageRow()
+            .background(.teal)
     }
 }
