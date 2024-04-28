7871b4b34b2f3feed586aa3f2a0b05bfed8b5ae6
exchange-wata <exchange.wata0907@gmail.com>
2024-04-28 10:07:22 +0900

    section7: jsonデータの変換

--- ChatViewWithSwiftUI/ViewModels/ChatViewModel.swift
+++ ChatViewWithSwiftUI/ViewModels/ChatViewModel.swift
@@ -8,5 +8,25 @@
 import Foundation
 
 class ChatViewModel {
-    
+    private func fetchChatData() -> [Chat] {
+        let filename = "chatData.json"
+        let data: Data
+        
+        guard let filePath = Bundle.main.url(forResource: filename, withExtension: nil) else {
+            fatalError("\(filename)が見つかりませんでした")
+        }
+        
+        
+        do {
+            data = try Data(contentsOf: filePath)
+        } catch {
+            fatalError("\(filename)のロードに失敗しました")
+        }
+        
+        do {
+           return try JSONDecoder().decode([Chat].self, from: data)
+        } catch {
+            fatalError("\(filename)を\(Chat.self)に変換できませんでした")
+        }
+    }
 }
